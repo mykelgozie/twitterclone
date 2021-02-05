3f20eeb836ee4f0d05bb855733f7c9f8ae752b0c
michael <mykelgozie@gmail.com>
2021-02-05 15:16:47 +0100

    validation of login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { user } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers:[user]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private user : user ) { }
 
   ngOnInit(): void {
+  }
+
+
+  onSubmit(form :NgForm){
+
+
+
+
+
+    if(form.value.email == this.user.email && form.value.password == this.user.password){
+
+        console.log("hey");
+    }
+    console.log(form.value.email);
+    console.log(form.value.password);
+    console.log(user);
   }
 
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,6 @@
+ export class user {
+
+  email = "mykelgozie@gmail.com";
+  password = "123456";
+
+ }
